d7ff2a3fa2e8381b25632a279aff6255f44a6840
Hài Dón <cathuys69@gmail.com>
2026-07-13 11:09:48 +0800

    Revert CameraController.cs to strictly match PDF slide code

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -7,34 +7,12 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If the camera is not parented, search for the UFO dynamically on every frame
-        if (transform.parent == null)
-        {
-            GameObject ufo = GameObject.FindWithTag("Player") ?? 
-                             GameObject.Find("UFO") ?? 
-                             GameObject.Find("UFO_0") ?? 
-                             GameObject.Find("UFO(Clone)");
-            
-            if (ufo == null)
-            {
-                UFOController controller = Object.FindFirstObjectByType<UFOController>();
-                if (controller != null) ufo = controller.gameObject;
-            }
-
-            if (ufo != null)
-            {
-                transform.SetParent(ufo.transform);
-                transform.localPosition = new Vector3(0, 0, -10);
-                transform.localRotation = Quaternion.identity;
-                Debug.Log("CameraController: Dynamically parented Main Camera to UFO.");
-            }
-        }
-
         // -- To prevent the random rotate of Camera
         // transform.eulerAngles = Vector3.zero;
         transform.eulerAngles = Vector2.zero;
